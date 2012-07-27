a9f196b6cda761b0a5f3322c5743cc8236469429
Rishabh Rao <rishabhsrao@yahoo.com>
2012-07-27 11:39:00 +0530

    Reorganized stuff.

--- js/modules/util/data/Constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- * Constants
- * The Constants module stores global constants required in the application.
- * @author See /humans.txt
- */
-define({
-});
--- js/modules/util/i18n/i18n_en.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- * Internationalization strings.
- * This i18n module stores all the i18n strings.
- * @author See /humans.txt
- */
-define({
-});
